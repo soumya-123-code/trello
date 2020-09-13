d5b61c7c2a58220232dfa8267e74ee87c02a3c2d
Aukpro198 <info@aukpro.com>
2020-09-14 01:29:54 +0530

    Updates

---
+++ precache-manifest.9a635088b12567a3e14f9fa8087f33cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a7f811e0067946a030f",
+    "url": "/trello/static/css/main.e7c9aba5.chunk.css"
+  },
+  {
+    "revision": "9a7f811e0067946a030f",
+    "url": "/trello/static/js/main.9a7f811e.chunk.js"
+  },
+  {
+    "revision": "a5dea7296e3ce2584698",
+    "url": "/trello/static/js/runtime~main.a5dea729.js"
+  },
+  {
+    "revision": "5a970e261d5aa7687d07",
+    "url": "/trello/static/js/2.5a970e26.chunk.js"
+  },
+  {
+    "revision": "fbf7f658122d4369528e4e04db12907a",
+    "url": "/trello/index.html"
+  }
+];
